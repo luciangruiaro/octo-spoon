19a2ca68379ae058b96d9757ecd7a96bcb1762b1
luciangruiaro <luciancgruia@gmail.com>
2023-01-27 22:22:33 +0200

    initial commit

---
+++ src/test/java/com/octospoon/octospoon/OctospoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octospoon.octospoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctospoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
